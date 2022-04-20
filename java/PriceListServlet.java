779824e150320f9b1f1bcbe44eb63358ed4d5be3
alekoChe <aleko2117bis@gmail.com>
2022-04-20 11:13:17 +0300

    create in class PriceList.java methods doGet and doPost

--- java/PriceListServlet.java
+++ java/PriceListServlet.java
@@ -7,15 +7,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "PriceListServlet", urlPatterns = "/priceList_servlet")
 public class PriceListServlet extends HttpServlet {
+    //List<Product>
 
     private static Logger logger = LoggerFactory.getLogger(PriceListServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //super.doGet(req, resp);
+        logger.info("OutputPriceList");
+
+        Product product = new Product();
+        product.fillingPriceList();
+
+        resp.getWriter().printf("<h1>Our products</h1>");
+        for (Product item : product.priceList) {
+            resp.getWriter().printf(item.getId() + " - " + item.getTitle() + " - " + item.getCost() + "\n");
+        }
+        //resp.getWriter().printf(String.valueOf(product.priceList));
     }
 
     @Override
